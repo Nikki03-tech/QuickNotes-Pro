613b34c78c28edc69c7607e554382a0b29acc51f
Nikitha Singh Raj Purohit <183282958+Nikki03-tech@users.noreply.github.com>
2026-05-01 20:15:29 +0530

    Update NavBar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,5 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import logo from "../assets/notes.png";
-
 export const NavBar = () => {
 	const user = useSelector((state) => state.auth.user);
-	const loadingCheckAuth = useSelector((state) => state.auth.loadingCheckAuth);
-	const loadingLogout = useSelector((state) => state.auth.loadingLogout);
-
-	if (loadingCheckAuth || loadingLogout) {
-		return null; // or loading indicator
-	}
 
 	return (
 		<nav className="p-4 text-white bg-gray-800">
@@ -19,38 +8,19 @@ export const NavBar = () => {
 					<img src={logo} alt="Logo" className="h-8 mr-2" />
 					<span className="font-bold">QuickNotes Pro</span>
 				</Link>
+
 				<ul className="flex items-center space-x-4">
 					{user ? (
 						<>
 							<p className="px-3 py-2">{user}</p>
-
 							<li>
-								<Link
-									to="/logout"
-									className="px-3 py-2 rounded hover:bg-gray-700"
-								>
-									Logout
-								</Link>
+								<Link to="/logout">Logout</Link>
 							</li>
 						</>
 					) : (
 						<>
-							<li>
-								<Link
-									to="/login"
-									className="px-3 py-2 rounded hover:bg-gray-700"
-								>
-									Login
-								</Link>
-							</li>
-							<li>
-								<Link
-									to="/signup"
-									className="px-3 py-2 rounded hover:bg-gray-700"
-								>
-									Signup
-								</Link>
-							</li>
+							<li><Link to="/login">Login</Link></li>
+							<li><Link to="/signup">Signup</Link></li>
 						</>
 					)}
 				</ul>
